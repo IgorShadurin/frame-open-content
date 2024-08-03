dd344a97a561f1cdfd4e5ee397256c37b4921d4d
Igor Shadurin <igor.shadurin@gmail.com>
2024-08-03 13:20:59 +0100

    feat: wowui

---
+++ wowui/src/Footer.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export function Footer() {
+  return (
+    <footer className="container py-5">
+      <div className="row">
+        <div className="col-12 col-md">
+          {/*<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor"*/}
+          {/*     stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="d-block mb-2" role="img"*/}
+          {/*     viewBox="0 0 24 24"><title>Product</title>*/}
+          {/*  <circle cx="12" cy="12" r="10" />*/}
+          {/*  <path*/}
+          {/*    d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" />*/}
+          {/*</svg>*/}
+          {/*<small className="d-block mb-3 text-body-secondary">&copy; 2017–2024</small>*/}
+        </div>
+        <div className="col-6 col-md">
+          <h5>Features</h5>
+          <ul className="list-unstyled text-small">
+            <li><a className="link-secondary text-decoration-none" href="/">AI Mini-Apps Design</a></li>
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Random feature</a></li>*/}
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Team feature</a></li>*/}
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Stuff for developers</a></li>*/}
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Another one</a></li>*/}
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Last time</a></li>*/}
+          </ul>
+        </div>
+        <div className="col-6 col-md">
+          <h5>GitHub</h5>
+          <ul className="list-unstyled text-small">
+            <li><a className="link-secondary text-decoration-none" href="https://github.com/DappyKit/" target="_blank">DappyKit</a></li>
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Resource</a></li>*/}
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Another resource</a></li>*/}
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Final resource</a></li>*/}
+          </ul>
+        </div>
+        <div className="col-6 col-md">
+          <h5>Contact</h5>
+          <ul className="list-unstyled text-small">
+            <li><a className="link-secondary text-decoration-none" href="mailto:dappykit@gmail.com">Email</a></li>
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Education</a></li>*/}
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Government</a></li>*/}
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Gaming</a></li>*/}
+          </ul>
+        </div>
+        <div className="col-6 col-md">
+          <h5>About</h5>
+          <ul className="list-unstyled text-small">
+            <li><a className="link-secondary text-decoration-none" href="https://warpcast.com/dappykit" target="_blank" rel="noopener">Team</a></li>
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Locations</a></li>*/}
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Privacy</a></li>*/}
+            {/*<li><a className="link-secondary text-decoration-none" href="#">Terms</a></li>*/}
+          </ul>
+        </div>
+      </div>
+    </footer>
+  )
+}
