a2fa8e7074374189b47e4f7165506834ddc32f9f
Igor Shadurin <igor.shadurin@gmail.com>
2024-08-12 17:58:41 +0100

    feat: show only on pc

--- mini.web4.build/src/Demo.tsx
+++ mini.web4.build/src/Demo.tsx
@@ -14,8 +14,8 @@ const state1 = {
       target: '.joy-submit',
       content: '✅ Submit the info to the AI so it can create the quiz.',
     },
-  ]
-};
+  ],
+}
 
 const state2 = {
   steps: [
@@ -23,8 +23,8 @@ const state2 = {
       target: '.joy-deploy',
       content: '💾 Save the application code on our servers to start using it.',
     },
-  ]
-};
+  ],
+}
 
 export function Demo() {
   const [quizData, setQuizData] = useState<QuizData | undefined>(undefined)
@@ -37,43 +37,50 @@ export function Demo() {
 
   return (
     <div className="container-fluid">
-      <Joyride
-        run={joyPart1}
-        steps={state1.steps}
-        continuous={true}
-      />
-      <Joyride
-        run={joyPart2}
-        steps={state2.steps}
-        continuous={true}
-      />
-      <div className="row">
-        <div className="col-9">
-          {quizData && <DemoDiagram quizData={quizData} />}
-          {!quizData && <div className="text-center" style={{
-            backgroundColor: '#f8fafc',
-            height: '100vh',
-            border: '1px solid lightgrey',
-            borderRadius: '3px'
-          }}>
-            <p className="mt-5">
-              ✉️ Send a message in the <strong>panel on the right</strong> to start creating your app.
-            </p>
+      <div className={'d-block d-md-none text-center'}>
+        <h1 className="mt-5">Use a PC for the Best Experience</h1>
+        <p>For a more comfortable experience, please visit this page using a desktop or laptop computer.</p>
+      </div>
 
-            <div className="mt-5 video-responsive">
-              <iframe width="900" height="507" src="https://www.youtube.com/embed/TmoItyYFdU8?si=9p3tz7Kf2myfBHdN"
-                      title="YouTube video player" frameBorder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                      referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-            </div>
-          </div>}
-        </div>
-        <div className="col-3">
-          <DemoChat onQuizData={async (data) => {
-            setQuizData(data)
-            setJoyPart1(false)
-            setJoyPart2(true)
-          }} />
+      <div className="d-none d-md-block">
+        <div className="row">
+          <Joyride
+            run={joyPart1}
+            steps={state1.steps}
+            continuous={true}
+          />
+          <Joyride
+            run={joyPart2}
+            steps={state2.steps}
+            continuous={true}
+          />
+          <div className="col-9">
+            {quizData && <DemoDiagram quizData={quizData} />}
+            {!quizData && <div className="text-center" style={{
+              backgroundColor: '#f8fafc',
+              height: '100vh',
+              border: '1px solid lightgrey',
+              borderRadius: '3px',
+            }}>
+              <p className="mt-5">
+                ✉️ Send a message in the <strong>panel on the right</strong> to start creating your app.
+              </p>
+
+              <div className="mt-5 video-responsive">
+                <iframe width="900" height="507" src="https://www.youtube.com/embed/TmoItyYFdU8?si=9p3tz7Kf2myfBHdN"
+                        title="YouTube video player" frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+              </div>
+            </div>}
+          </div>
+          <div className="col-3">
+            <DemoChat onQuizData={async (data) => {
+              setQuizData(data)
+              setJoyPart1(false)
+              setJoyPart2(true)
+            }} />
+          </div>
         </div>
       </div>
     </div>
